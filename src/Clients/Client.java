a30a0f231eefe148ebb269a0c3fbbde0fd81322b
Diego Fernández Barrera <bigomby@gmail.com>
2014-04-29 12:12:43 +0200

    Añadido el esqueleto de Servidor y Cliente.

---
+++ src/Clients/Client.java
@@ -0,0 +1,13 @@
+package Clients;
+
+/*
+ * Cliente
+ * 
+ * Se conecta con un servidor para ver los dispositivos que hay activos. Puede enviar órdenes
+ * para controlarlos o suscribirse a notificaciones de los dispositivos que los soporten.
+ */
+public class Client {
+	public static void main(String[] args) {
+		// TODO
+	}
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,15 @@
+package Server;
+
+/*
+ * Servidor
+ * 
+ * Recibe las conexiones tanto de los clientes como de los diferentes dispositivos y se encarga
+ * de reenviar las peticiones. También recibe notificaciones de los dispositivos en el caso  de 
+ * que las envíen y las envía a los clientes que estén suscritos a éstas.
+ */
+
+public class Server {
+	public static void main(String[] args) {
+		// TODO
+	}
+}
