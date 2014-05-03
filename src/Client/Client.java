2cfa5296868da3b61eadbf45e0d31335f5c73d64
Diego Fernández Barrera <bigomby@gmail.com>
2014-05-03 15:43:07 +0200

    Cambiado el nombre del paquete "Clients" a "Client" y implementando un cliente "Hola Mundo".

---
+++ src/Client/Client.java
@@ -0,0 +1,41 @@
+package Client;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+import Services.ServiceDomotic;
+
+/*
+ * Cliente
+ * 
+ * Se conecta con un servidor para ver los dispositivos que hay activos. Puede enviar órdenes
+ * para controlarlos o suscribirse a notificaciones de los dispositivos que los soporten.
+ */
+
+class Client {
+
+	static public void main(String args[]) {
+		System.setProperty("java.security.policy","file:policies.policy");
+		
+		if (args.length != 1) {
+			System.err.println("Uso: Client <Server>");
+			return;
+		}
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+
+		try {
+			ServiceDomotic srv = (ServiceDomotic) Naming.lookup("//"
+					+ args[0] + ":" + 54321 + "/Domotic");
+
+			srv.test();
+		} catch (RemoteException e) {
+			System.err.println("Error de comunicacion: " + e.toString());
+		} catch (Exception e) {
+			System.err.println("Excepcion en Client:");
+			e.printStackTrace();
+		}
+	}
+}
