eea121c53da4b2218ea010ba7ee2d74c8cd246e5
Diego Fernández Barrera <bigomby@gmail.com>
2014-05-03 19:22:32 +0200

    Implementados dos servidores, uno para clientes y otro para dispositivos funcionando en hilos diferentes.

---
+++ src/Server/RMIClientServer.java
@@ -0,0 +1,68 @@
+package Server;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.util.Enumeration;
+
+
+public class RMIClientServer implements Runnable {
+	
+	ClientServiceImpl clientService;
+	
+	RMIClientServer (ClientServiceImpl clientService){
+		this.clientService = clientService;
+	}
+	
+	public void run() {
+		String ip = getIP();
+		System.setProperty("java.security.policy", "file:policies.policy");
+		System.setProperty("java.rmi.server.hostname", ip);
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+		try {
+			
+			Naming.rebind("//" + ip + ":" + "54321" + "/Client", clientService);
+			
+			System.out.println("Servidor de clientes iniciado en " + ip);
+			
+		} catch (RemoteException e) {
+			System.out.println("Error de comunicacion: " + e.toString());
+			System.exit(1);
+		} catch (Exception e) {
+			System.out.println("Excepcion en Domotic:");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	private String getIP() {
+		String ip = null;
+
+		@SuppressWarnings("rawtypes")
+		Enumeration d;
+		try {
+			d = NetworkInterface.getNetworkInterfaces();
+
+			while (d.hasMoreElements()) {
+				NetworkInterface n = (NetworkInterface) d.nextElement();
+				Enumeration<InetAddress> ee = n.getInetAddresses();
+				while (ee.hasMoreElements()) {
+					InetAddress i = (InetAddress) ee.nextElement();
+					if (i.getHostAddress().toString().startsWith("192.168.1.")) {
+						ip = i.getHostAddress();
+					}
+				}
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		return ip;
+	}
+}
---
+++ src/Server/RMIDeviceServer.java
@@ -0,0 +1,68 @@
+package Server;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.util.Enumeration;
+
+
+public class RMIDeviceServer implements Runnable {
+	
+	DeviceServiceImpl deviceService;
+	
+	RMIDeviceServer(DeviceServiceImpl deviceService){
+		this.deviceService = deviceService;
+	}
+	
+	public void run() {
+		String ip = getIP();
+		System.setProperty("java.security.policy", "file:policies.policy");
+		System.setProperty("java.rmi.server.hostname", ip);
+
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new RMISecurityManager());
+		}
+		try {
+			DeviceServiceImpl devices = new DeviceServiceImpl();
+			Naming.rebind("//" + ip + ":" + "54321" + "/Device", devices);
+			
+			System.out.println("Servidor de dispositivos iniciado en " + ip);
+			
+		} catch (RemoteException e) {
+			System.out.println("Error de comunicacion: " + e.toString());
+			System.exit(1);
+		} catch (Exception e) {
+			System.out.println("Excepcion en Domotic:");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	private String getIP() {
+		String ip = null;
+
+		@SuppressWarnings("rawtypes")
+		Enumeration d;
+		try {
+			d = NetworkInterface.getNetworkInterfaces();
+
+			while (d.hasMoreElements()) {
+				NetworkInterface n = (NetworkInterface) d.nextElement();
+				Enumeration<InetAddress> ee = n.getInetAddresses();
+				while (ee.hasMoreElements()) {
+					InetAddress i = (InetAddress) ee.nextElement();
+					if (i.getHostAddress().toString().startsWith("192.168.1.")) {
+						ip = i.getHostAddress();
+					}
+				}
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+
+		return ip;
+	}
+}
